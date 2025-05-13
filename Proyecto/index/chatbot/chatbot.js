fe81421ba46bd4b8ea94f41a605f17c5ac9605ff
toreritta <ahtziri.berlanga@gmail.com>
2025-05-12 21:01:24 -0600

    Add files via upload

---
+++ Proyecto/index/chatbot/chatbot.js
@@ -0,0 +1,31 @@
+
+//                  MENU
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+//                  SLIDER
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const indicator = document.querySelector(".indicator");
+
+[openMenuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("open");
+        menu.style.transition = "transform 0.5s ease";
+    });
+});
+
+menu.addEventListener("transitioned", function() {
+    this.removeAttribute("style");
+});
+
+
+//              DROPDOWN
+
+menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
+    arrow.addEventListener("click", function(){
+        this.closest(".dropdown").classList.toggle("active");
+    });
+});
