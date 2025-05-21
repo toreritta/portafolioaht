dd75051d357ebe7030ac77ee0f00d66d684d65ba
toreritta <ahtziri.berlanga@gmail.com>
2025-05-20 23:18:23 -0600

    Update actualidad.js

--- Proyecto/index/actualidad/actualidad.js
+++ Proyecto/index/actualidad/actualidad.js
@@ -1,27 +1,9 @@
-//                  PAQUETES A CARGAR
-
-google.charts.load('current', {'packages':['table']});
 
 //                  MENU
 
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
-var tabla1 = document.getElementById('tabla1');
-var tabla2 = document.getElementById('tabla2');
-
-
-[openMenuBtn, closeMenuBtn].forEach((btn) => {
-    btn.addEventListener("click", () => {
-        menu.classList.toggle("open");
-        menu.style.transition = "transform 0.5s ease";
-    });
-});
-
-menu.addEventListener("transitioned", function() {
-    this.removeAttribute("style");
-});
-
 //                  SLIDER
 const slides = document.querySelector(".slider").children;
 const prev = document.querySelector(".prev");
@@ -38,6 +20,7 @@ const indicator = document.querySelector(".indicator");
 menu.addEventListener("transitioned", function() {
     this.removeAttribute("style");
 });
+
 
 //              DROPDOWN
 
