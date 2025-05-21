13d5eb984a1d67ad47caaea1168161f047f13fb6
toreritta <ahtziri.berlanga@gmail.com>
2025-05-20 23:14:17 -0600

    Add files via upload

---
+++ Proyecto/index/blog/interaccionjava.js
@@ -0,0 +1,28 @@
+
+//                  MENU
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+
+[openMenuBtn, closeMenuBtn].forEach((btn) => 
+    {
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("open");
+        menu.style.transition = "transform 0.5s ease";
+    });
+});
+
+menu.addEventListener("transitioned", function() 
+{
+    this.removeAttribute("style");
+});
+
+
+//              DROPDOWN
+
+menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
+    arrow.addEventListener("click", function(){
+        this.closest(".dropdown").classList.toggle("active");
+    });
+});
